4acb7025f071344263023d0133ad566242fac254
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-01-27 23:45:57 +0200

    migration to vite

--- src/script.js
+++ src/script.js
@@ -1,10 +1,31 @@
-import "./style.css";
+// Author: Michael Kolesidis
+// Title: synthétique
+
+// Copyright (c) 2023 Michael Kolesidis - https://michaelkolesidis.com/
+// I am the sole copyright owner of this Work.
+//
+// Reproduction of any of the artwork on this website
+// for commercial use is not permitted without first
+// receiving written permission from the artist. You
+// cannot host, display, distribute or share this Work
+// in any form, including physical and digital. You
+// cannot use this Work in any commercial or non-commercial
+// product, website or project. You cannot sell this Work and
+// you cannot mint an NFTs of it.
+
+// Under the Copyright Law, it is fair use to reproduce a single
+// copy for personal or educational purposes, provided that no
+// changes are made to the content and provided that a copyright
+// notice attesting to the content is attached to the reproduction.
+// Beyond that, no further copies of works of art may be made or
+// distributed on this website without written permission.
+
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
 /**
- * Basicsm
+ * Basics
  */
 // HTML Title and Favicon
 document.title = "synthétique";
@@ -101,7 +122,8 @@ helpPanel.setAttribute("id", "help-panel");
 helpPanel.innerHTML = `
 key<br>
 S : start/stop music<br>
-H : open controls`;
+H : open controls<br>
+M : mark fisher`;
 mainMenu.appendChild(helpPanel);
 
 asterisk.addEventListener("click", () => {
